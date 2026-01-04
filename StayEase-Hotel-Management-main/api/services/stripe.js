85250411567c82ce35969abb9b091a51fb5e0298
Rafsan <mahir.muntasir.rafsan@g.bracu.ac.bd>
2026-01-04 23:57:44 +0600

    change the deleted files

--- StayEase-Hotel-Management-main/api/services/stripe.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // keep your key in .env
-
-export default stripe;
